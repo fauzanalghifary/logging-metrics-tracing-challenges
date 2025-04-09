3dfc233250bdbdfbdeab7a19ca7b245855638716
fauzanalghifary <m.fauzanghifary@gmail.com>
2025-04-09 18:32:27 +0700

    add OpenTelemetry Tracing

--- go.mod
+++ go.mod
@@ -6,12 +6,14 @@ toolchain go1.23.7
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.1 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -24,11 +26,19 @@ require (
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.35.0 // indirect
 	go.opentelemetry.io/otel/exporters/prometheus v0.57.0 // indirect
 	go.opentelemetry.io/otel/metric v1.35.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.35.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.35.0 // indirect
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/grpc v1.71.1 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 )
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"net/http"
 	"os"
 	"os/signal"
@@ -23,9 +24,16 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/trace"
+	oteltrace "go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
 )
 
 var meter otelmetric.Meter
+var tracer oteltrace.Tracer
 var reqHistogram otelmetric.Float64Histogram
 
 func main() {
@@ -87,6 +95,26 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to create histogram")
 	}
+
+	tc := otlptracegrpc.NewClient(
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint("localhost:4317"),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()),
+	)
+
+	traceExporter, err := otlptrace.New(ctx, tc)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to create trace exporter")
+	}
+
+	bsp := trace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithResource(res),
+		trace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	tracer = otel.Tracer("logging-challenge")
 
 	r := mux.NewRouter()
 	r.Use(middleware)
@@ -160,8 +188,20 @@ func middleware(next http.Handler) http.Handler {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+
+	ctx, span := tracer.Start(
+		ctx, "handler", oteltrace.WithAttributes(
+			attribute.String("user_id", "foo"),
+		),
+	)
+	defer span.End()
+
 	log := log.Ctx(ctx).With().Str("func", "handler").Logger()
 	name := r.URL.Query().Get("name")
+
+	span.SetAttributes(
+		attribute.String("name", name),
+	)
 
 	log.Debug().
 		Str("name", name).
@@ -169,10 +209,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	res, err := greeting(ctx, name)
 	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		span.RecordError(err)
 		log.Error().Err(err).Msg("failed to process request")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	span.AddEvent(
+		"greeting function is executed",
+		oteltrace.WithAttributes(
+			attribute.String("result", "ok"),
+		),
+	)
 
 	log.Info().Msg("processed request successfully")
 
@@ -180,10 +229,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func greeting(ctx context.Context, name string) (string, error) {
+	_, span := tracer.Start(ctx, "greeting")
+	defer span.End()
+
 	log := log.Ctx(ctx).With().Str("func", "greeting").Logger()
 	if len(name) < 5 {
 		log.Warn().Msgf("name is too short %s", name)
-		return fmt.Sprintf("Hello %s! Your name is to short\n", name), nil
+		return "", fmt.Errorf("Hello %s! Your name is to short\n", name)
 	}
 
 	log.Debug().Msgf("name is long enough: %s", name)
